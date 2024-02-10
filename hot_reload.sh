cc384175a5c4180614f723a75b9f974b05f62e62
jaennil <jaennil@yandex.ru>
2024-02-10 23:23:56 +0300

    8-9, 4custom; reorganization

--- hot_reload.sh
+++ hot_reload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-inotifywait -q -m -e close_write --format %e cgw.tex |
+inotifywait -q -m -e close_write --format %e $1 |
 while read events; do
-	pdflatex -interaction=nonstopmode cgw
+	pdflatex -interaction=nonstopmode $1
 done
 
